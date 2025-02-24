79c6640103a2360b8582bc08f5e4c4958e1dab1a
Squikys <kaustavmahata@gmail.com>
2025-02-25 02:12:35 +0530

    deleted some files

--- AI/config.py
+++
@@ -1,9 +0,0 @@
-import cloudinary
-import cloudinary.uploader
-
-# Configure Cloudinary with your credentials
-cloudinary.config(
-    cloud_name="diversion",
-    api_key="186388281882135",
-    api_secret="y-RCvi5R2U-GlOBR6XCWdiRW7Wo"
-)
